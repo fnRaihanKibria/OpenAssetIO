34619b3572d0625c4f91fded6a99b45bbfce1188
David Feltell <david.feltell@foundry.com>
2022-04-01 18:04:31 +0100

    squash: Add trompeloeil mocking library
    Signed-off-by: David Feltell <david.feltell@foundry.com>

--- tests/openassetio-core-c/managerAPI/CManagerInterfaceTest.cpp
+++ tests/openassetio-core-c/managerAPI/CManagerInterfaceTest.cpp
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2013-2022 The Foundry Visionmongers Ltd
 #include <catch2/catch.hpp>
+#include <catch2/trompeloeil.hpp>
 
 #include <managerAPI/CManagerInterface.hpp>
